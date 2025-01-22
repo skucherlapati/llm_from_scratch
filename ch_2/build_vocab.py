57a6fe14c5bc416127b49ec58d3eae5005e60988
sumedha <sumedha@sironamedical.com>
2025-01-22 14:06:44 -0800

    add uv and linters

--- ch_2/build_vocab.py
+++ ch_2/build_vocab.py
@@ -10,6 +10,8 @@ logging.basicConfig(encoding="utf-8", level=logging.DEBUG, format='%(asctime)s -
 RE_TOKENIZER = re.compile(r"([,.:;?_!\"()\']|--|\s)")
 THE_VERDICT_URL = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/refs/heads/main/ch02/01_main-chapter-code/the-verdict.txt"
 THE_VERDICT_FILE_PATH = "the-verdict.txt"
+UNK_TOKEN = "<|unk|>"
+DOC_SEP_TOKEN = "<|endoftext|>"
 
 
 def download_text_data(url: str, filepath: str):
@@ -49,13 +51,29 @@ def create_vocab(preprocessed: list[str], debug: bool = True):
     return vocab
 
 
-def main(url: str = THE_VERDICT_URL, filepath: str = THE_VERDICT_FILE_PATH):
+def create_vocab_with_spl_tokens(preprocessed: list[str], debug: bool = True):
+    """Create vocab from list of words."""
+    all_words = sorted(set(preprocessed))
+    all_words.extend([DOC_SEP_TOKEN, UNK_TOKEN])
+    vocab_size = len(all_words)
+    logger.info(f"Vocab size: {vocab_size}")
+    vocab = {token: ii for ii, token in enumerate(all_words)}
+    if debug:
+        for token, i in vocab.items():
+            logger.debug(token)
+            if i >= 50:
+                break
+    return vocab
+
+def main(url: str = THE_VERDICT_URL, filepath: str = THE_VERDICT_FILE_PATH, add_special_tokens: bool = False):
     """Build vocab from the verdict text."""
     download_text_data(url, filepath)
     raw_text = read_text_file(filepath)
     words = convert_text_to_words(raw_text)
-    vocab = create_vocab(words)
-    return vocab
+    if add_special_tokens:
+        return create_vocab_with_spl_tokens(words)
+    else:
+        return create_vocab(words)
 
 
 if __name__ == "__main__":
--- ch_2/tokenizers.py
+++ ch_2/tokenizers.py
@@ -28,10 +28,32 @@ class SimpleTokenizerV1:
         text = " ".join([self.int_to_str[i] for i in ids])
         text = RE_RMV_PUNCT_SPACES.sub(r"\1", text)
         return text
+
+
+class SimpleTokenizerV2:
+    """Handles unseen words in text that are not present in vocab."""
+    def __init__(self, vocab: dict[str, int], unk_token: str):
+        """Init vocab."""
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s, i in vocab.items()}
+        self.unk_token = unk_token
     
+    def encode(self, text: str) -> list[int]:
+        """Text to token ints."""
+        preprocessed = convert_text_to_words(text)
+        ids = [self.str_to_int[s] if s in self.str_to_int else self.str_to_int[self.unk_token] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids: list[int]) -> str:
+        """Token ints to texts."""
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = RE_RMV_PUNCT_SPACES.sub(r"\1", text)
+        return text
+
+
 
 if __name__ == "__main__":
-    from build_vocab import main
+    from build_vocab import main, UNK_TOKEN, DOC_SEP_TOKEN
     vocab = main()
     tokenizer = SimpleTokenizerV1(vocab=vocab)
     text = """"It's the last he painted you know,"
@@ -39,3 +61,17 @@ if __name__ == "__main__":
     ids = tokenizer.encode(text)
     logger.debug(f"Input: {text}")
     logger.debug(f"Tokenized output: {ids}")
+    logger.debug(f"Decoder output: {tokenizer.decode(ids)}")
+
+    logger.debug("-"*80)
+    text1 = "Hello, do you like tea?"
+    text2 = "In the sunlit terraces of the palace."
+    text = f" {DOC_SEP_TOKEN} ".join([text1, text2])
+
+    vocab = main(add_special_tokens=True)
+    tokenizer = SimpleTokenizerV2(vocab=vocab, unk_token=UNK_TOKEN)
+
+    ids = tokenizer.encode(text)
+    logger.debug(f"Input: {text}")
+    logger.debug(f"Tokenized output: {ids}")
+    logger.debug(f"Decoder output: {tokenizer.decode(ids)}")
